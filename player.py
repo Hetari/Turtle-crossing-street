46149194917326b00b03f222f57f533d99ec4316
Ebraheem Hetari <108173819+Hetari@users.noreply.github.com>
2023-02-05 17:45:52 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,5 +1,4 @@
 from turtle import Turtle
-import time
 
 
 STARTING_POSITION = (0, -280)
@@ -8,31 +7,22 @@ FINISH_LINE_Y = 280
 
 
 class Player(Turtle):
+    level = 0
 
     def __init__(self):
-        self.level = 1
-        print(f"Level: {self.level}")
         super().__init__()
-        # self.color("black")
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
     def up(self):
         if self.ycor() <= 280:
             self.forward(MOVE_DISTANCE)
 
-    def winning(self, level):
-        t = '0.'
-        for digit in range(level):
-            if digit == level - 1:
-                t += '1'
-            else:
-                t += '0'
+    def is_finish_laine(self):
+        return self.ycor() > FINISH_LINE_Y
 
-        time.sleep(float(t))
-        if self.ycor() > 280.0:
-            self.level += 1
-            print(f"Level: {self.level}")
-            self.goto(STARTING_POSITION)
+    def go_to_start(self):
+        self.level += 1
+        self.goto(STARTING_POSITION)
